db64445acab8997bc2977847cc2800e93769be1a
jr010014 <46798756+jr010014@users.noreply.github.com>
2020-04-02 14:23:50 -0400

    Added Basement Scene

---
+++ Assets/Scripts/FurnaceBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnaceBehavior : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit");
+    }
+}
