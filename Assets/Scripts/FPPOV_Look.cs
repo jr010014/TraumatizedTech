6bec58914a53b8a1e4b22633c3680aea4805c01c
jr010014 <46798756+jr010014@users.noreply.github.com>
2020-04-02 13:36:40 -0400

    Initial commit

---
+++ Assets/Scripts/FPPOV_Look.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPPOV_Look : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothLook;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject avatar;
+
+    void Start()
+    {
+        avatar = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothLook.x = Mathf.Lerp(smoothLook.x, mouseDelta.x, 1f / smoothing);
+        smoothLook.y = Mathf.Lerp(smoothLook.y, mouseDelta.y, 1f / smoothing);
+        mouseLook += smoothLook;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        avatar.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, avatar.transform.up);
+
+        //transform.localRotation = Quaternion.AngleAxis(-mouseDelta.y, Vector3.right);
+        //avatar.transform.localRotation = Quaternion.AngleAxis(mouseDelta.x, avatar.transform.up);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TraumatizedTech
+ 
