fecbab161525d8b266a416f30b86ee313b634777
jr010014 <46798756+jr010014@users.noreply.github.com>
2020-04-12 14:30:15 -0400

    Added clock and heartbeat sounds

---
+++ Assets/Scripts/DetermineHeartbeat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetermineHeartbeat : MonoBehaviour
+{
+    AudioSource heartbeat;
+
+    private void Start()
+    {
+        heartbeat = GetComponent<AudioSource>();
+
+    }
+    private void Update()
+    {
+        //if (Vector3.Distance(collider.transform.position, gameObject.transform.position) > 2f)
+        //{
+            //if far enough, stop heartbeat
+        //    heartbeat.Stop();
+        //}
+    }
+
+    public void OnCollisionEnter(Collision collider)
+    {
+        //evaluate distance between player and objects that induce heartbeat
+        if ( Vector3.Distance(collider.transform.position, gameObject.transform.position) < 2f)
+        {
+            //if close enough, induce heartbeat
+            
+            heartbeat.Play();
+        }
+        
+    }
+}
