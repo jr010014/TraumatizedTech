b268be1e95ba62cca75d158ca33a4e0b3562fba8
jr010014 <46798756+jr010014@users.noreply.github.com>
2020-04-11 20:10:24 -0400

    Began clock animation

---
+++ Assets/Scripts/GrandClock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandClock : MonoBehaviour
+{
+    public GameObject bigHand;
+    public GameObject littleHand;
+    public GameObject Pendulum;
+    string oldSeconds;
+
+    void Update()
+    {
+        string seconds = System.DateTime.UtcNow.ToString("ss");
+
+        if(seconds != oldSeconds)
+        {
+            UpdateTimer();
+        }
+        oldSeconds = seconds;
+    }
+
+    void UpdateTimer()
+    {
+        int secondsInt = int.Parse(System.DateTime.UtcNow.ToString("ss"));
+        int minutesInt = int.Parse(System.DateTime.UtcNow.ToString("mm"));
+        int hoursInt = int.Parse(System.DateTime.UtcNow.ToString("hh"));
+
+
+        print(secondsInt);
+        print(minutesInt);
+        print(hoursInt);
+
+        iTween.RotateTo(littleHand, iTween.Hash("x", secondsInt * 6 * 1, "time", 1, "easetype", "easeOutQuint"));
+    }
+}
