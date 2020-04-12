dc3d46e011e3ae038ac2ca54767403ae76d424d3
jr010014 <46798756+jr010014@users.noreply.github.com>
2020-04-11 22:05:06 -0400

    more clockwork

--- Assets/Scripts/GrandClock.cs
+++ Assets/Scripts/GrandClock.cs
@@ -6,31 +6,64 @@ public class GrandClock : MonoBehaviour
 {
     public GameObject bigHand;
     public GameObject littleHand;
-    public GameObject Pendulum;
-    string oldSeconds;
+    public GameObject pendulum;
 
-    void Update()
+
+    private void Update()
     {
-        string seconds = System.DateTime.UtcNow.ToString("ss");
-
-        if(seconds != oldSeconds)
+        littleHand.transform.Rotate(new Vector3(360f, 0f, 0f) * Time.deltaTime);
+        bigHand.transform.Rotate(new Vector3(30f, 0f, 0f) * Time.deltaTime);
+        if (Mathf.Abs(pendulum.transform.eulerAngles.x) > 4)
         {
-            UpdateTimer();
+            pendulumSwing();
         }
-        oldSeconds = seconds;
+
     }
 
-    void UpdateTimer()
+    public void pendulumSwing()
     {
-        int secondsInt = int.Parse(System.DateTime.UtcNow.ToString("ss"));
-        int minutesInt = int.Parse(System.DateTime.UtcNow.ToString("mm"));
-        int hoursInt = int.Parse(System.DateTime.UtcNow.ToString("hh"));
+        if (pendulum.transform.eulerAngles.x >= 4)
+        {
+            pendulum.transform.Rotate(new Vector3(-6f, 0f, 0f) * Time.deltaTime);
 
-
-        print(secondsInt);
-        print(minutesInt);
-        print(hoursInt);
-
-        iTween.RotateTo(littleHand, iTween.Hash("x", secondsInt * 6 * 1, "time", 1, "easetype", "easeOutQuint"));
+        }
+        else if (pendulum.transform.eulerAngles.x < -4)
+        {
+            pendulum.transform.Rotate(new Vector3(6f, 0f, 0f) * Time.deltaTime);
+        }
     }
-}
+
+        /*
+       public GameObject bigHand;
+       public GameObject littleHand;
+       public GameObject Pendulum;
+       string oldSeconds;
+
+       void Update()
+       {
+           string seconds = System.DateTime.UtcNow.ToString("ss");
+
+          if(seconds != oldSeconds)
+          {
+            UpdateTimer();
+          }
+          oldSeconds = seconds;
+       }
+
+       void UpdateTimer()
+       {
+           int secondsInt = int.Parse(System.DateTime.UtcNow.ToString("ss"));
+           int minutesInt = int.Parse(System.DateTime.UtcNow.ToString("mm"));
+           int hoursInt = int.Parse(System.DateTime.UtcNow.ToString("hh"));
+
+
+           print(secondsInt);
+           print(minutesInt);
+           print(hoursInt);
+
+           iTween.RotateTo(littleHand, iTween.Hash("x", secondsInt * 6 * -1, "time", 1, "easetype", "easeOutQuint"));
+           iTween.RotateTo(bigHand, iTween.Hash("x", minutesInt * 6 * -1, "time", 1, "easetype", "easeOutQuint"));
+
+       }
+       */
+    }
